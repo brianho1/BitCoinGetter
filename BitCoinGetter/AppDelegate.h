3b935885b4f013e97aa803ccd6bb1e3805462c22
kumok <bhh@byu.edu>
2015-02-07 10:48:02 -0700

    Initial Commit

---
+++ BitCoinGetter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BitCoinGetter
+//
+//  Created by Duc Ho on 2/7/15.
+//  Copyright (c) 2015 self. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
