231d8cbe7b0c0732e84e66159b4b09acc0c10b40
kumok <bhh@byu.edu>
2015-02-08 19:24:29 -0700

    graph

---
+++ BitCoinGetter/BCGraphViewController.h
@@ -0,0 +1,14 @@
+//
+//  BCGraphViewController.h
+//  BitCoinGetter
+//
+//  Created by Duc Ho on 2/7/15.
+//  Copyright (c) 2015 self. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BCGraphViewController : UIViewController
+
+@property (strong, nonatomic) NSDictionary *dictionaryData;
+@end
--- BitCoinGetter/ViewController.m
+++ BitCoinGetter/ViewController.m
@@ -8,10 +8,13 @@
 
 #import "ViewController.h"
 #import "BCController.h"
-
+#import "SHLineGraphView.h"
+#import "SHPlot.h"
+#import "BCGraphViewController.h"
 
 @interface ViewController ()
 @property (weak, nonatomic) IBOutlet UILabel *currentPriceLabel;
+@property (strong, nonatomic) BCBitCoin * bit;
 
 @end
 
@@ -20,15 +23,31 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    
     [[BCController sharedInstance] getCurrentPrice:^(BCBitCoin *bitcoin) {
         NSLog(@"%@",bitcoin.currentPrice);
         self.currentPriceLabel.text = bitcoin.currentPrice;
         NSLog(@"%@",bitcoin.pastPrices);
         NSDictionary *listPrices = bitcoin.pastPrices[@"bpi"];
-        NSLog(@"%@", listPrices);
-    }];
+        NSLog(@"%@",listPrices);
+        //test graph here
+        
+        self.bit = bitcoin;
+        }];
+    
+
     
 }
+
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    if ([segue.identifier isEqualToString:@"showGraphDetail"]) {
+
+        BCGraphViewController *destViewController = segue.destinationViewController;
+        destViewController.dictionaryData = self.bit.pastPrices[@"bpi"];
+    }
+}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
